fd609346081903f19c2ab823d1ec356a0cc277b4
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-07-31 23:03:28 -0400

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -1,12 +1,12 @@
 import requests
 import smtplib
 from bs4 import BeautifulSoup
+URL = input("Hey, enter a newegg URL you would like to set an alert for, then press Enter: ")
 
-URL = 'https://www.newegg.ca/p/1A0-0077-001A9?Item=9SIAP44EZZ1057'
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
 
-
+# Method to send email when price criteria is met
 def notify():
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -14,26 +14,36 @@ def notify():
     server.ehlo()
     server.login('saleemusama1@gmail.com', 'jixhgovxcpfzdjbf')
     subject = '[NEWEGG] Price Alert!'
-    body = 'Product Link: ' + URL
+    body = 'Hey! The price has gone down. Its now ' + str(price) + ', heres the product link: ' + URL
 
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail('saleemusama1@gmail.com', 'usama.saleem@hotmail.com', msg)
-    print('Email sent')
+    server.sendmail('saleemusama1@gmail.com', recipient, msg)
+    print('PRICE IS LOWER! Email sent!')
     server.quit()
 
-
+# Scraping data from site URL 
 page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-
 price = soup.find_all('li', class_='price-current')
 output = []
 
 for soup in price:
     output.append(soup.find("strong").get_text())
     output.append(soup.find("sup").get_text())
-price=float((output[0]+output[1]))
+price=((output[0]+output[1]))
+price = float(price.replace(',', ''))
 print(price)
 
-if(price < 100):
+priceToCheck = input("\nIt seems the current price is " + str(price) + ", what would you like the price to be? Ex: 420.69: ")
+priceToCheck = float(priceToCheck)
+price = float(price)
+recipient = input("\nEnter the email you want the alert to be sent to: ")
+
+# Criteria to send email
+if(price > priceToCheck):
     notify()
+else: 
+    print("Gotcha. It's currently above what you want to purchase it for, so i'll notify you when its at " + str(priceToCheck) + " or lower! :)")
+    
+
